5cd1de860634e55a9a6d7f7e63f58f5fe7211a6b
gabo-x86 <hurtadogabriel58@gmail.com>
2022-12-07 07:49:35 -0400

    initial routes implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPatitoComponent } from './components/forms/add-patito/add-patito.component';
+import { MainViewComponent } from './components/forms/main-view/main-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: MainViewComponent},
+    {path: 'add-patito', component: AddPatitoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TableComponent } from './components/table/table.component';
+import { AddPatitoComponent } from './components/forms/add-patito/add-patito.component';
+import { MainViewComponent } from './components/forms/main-view/main-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    ButtonComponent
+    ButtonComponent,
+    TableComponent,
+    AddPatitoComponent,
+    MainViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
